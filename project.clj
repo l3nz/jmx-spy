6dd26c690f131f8c6db5a54af6753c937b4acc98
lenz <lenz@mac>
2019-04-27 11:18:02 +0200

    Ver 0.1.2

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject jmxspy "0.1.1"
-  :description
-  "Downloads JMS statistics and pushes them to Influx"
-  :url
-  "https://github.com/l3nz/jmx-spy"
-  :license
-  {:name "Eclipse Public License",
-   :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject jmxspy "0.1.2"
+  :description "Downloads JMX statistics and pushes them to Influx"
+  :url  "https://github.com/l3nz/jmx-spy"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
   :dependencies
   [[org.clojure/clojure "1.9.0"]
    [cli-matic "0.3.6"]
@@ -14,9 +12,12 @@
    [lenz/capacitor "0.6.0.2"]
    [orchestra "2019.02.06-1"]
    [cheshire "5.8.0"]]
+  
   :main jmxspy.core
   :aot :all
-  :global-vars {*warn-on-reflection* false, *assert* true}
+
+  :global-vars {*warn-on-reflection* false, 
+                *assert* true}
   :plugins [[lein-codox "0.10.3"]
             [lein-eftest "0.5.1"]
             [lein-cljfmt "0.5.7"]]
