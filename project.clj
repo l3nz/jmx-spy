98acbe1c0ef7646484b2ff7fb51fdad23b0c31d7
lenz <lenz@mac>
2019-04-27 08:56:40 +0200

    Fixing #1 - Print number of beans read on STDERR
    While I was at it, a did a bit of clean-up.

--- project.clj
+++ project.clj
@@ -1,27 +1,27 @@
 (defproject jmxspy "0.1.1"
-:description
- "Downloads JMS statistics and pushes them to Influx"
- :url
- "https://github.com/l3nz/jmx-spy"
- :license
- {:name "Eclipse Public License",
-  :url "http://www.eclipse.org/legal/epl-v10.html"}
- :dependencies
- [[org.clojure/clojure "1.9.0"]
-  [cli-matic "0.3.6"]
-  [say-cheez "0.1.1"]
-  [org.clojure/java.jmx "0.3.4"]
-  [lenz/capacitor "0.6.0.2"]
-  [orchestra "2019.02.06-1"]
-  [cheshire "5.8.0"]]
- :main jmxspy.core
- :aot :all
- :global-vars {*warn-on-reflection* false, *assert* true}
- :plugins [[lein-codox "0.10.3"]
-           [lein-eftest "0.5.1"]
-           [lein-cljfmt "0.5.7"]]
- :eftest
- {:multithread? true,
-  :report eftest.report.junit/report,
-  :report-to-file "target/junit.xml"})
+  :description
+  "Downloads JMS statistics and pushes them to Influx"
+  :url
+  "https://github.com/l3nz/jmx-spy"
+  :license
+  {:name "Eclipse Public License",
+   :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies
+  [[org.clojure/clojure "1.9.0"]
+   [cli-matic "0.3.6"]
+   [say-cheez "0.1.1"]
+   [org.clojure/java.jmx "0.3.4"]
+   [lenz/capacitor "0.6.0.2"]
+   [orchestra "2019.02.06-1"]
+   [cheshire "5.8.0"]]
+  :main jmxspy.core
+  :aot :all
+  :global-vars {*warn-on-reflection* false, *assert* true}
+  :plugins [[lein-codox "0.10.3"]
+            [lein-eftest "0.5.1"]
+            [lein-cljfmt "0.5.7"]]
+  :eftest
+  {:multithread? true,
+   :report eftest.report.junit/report,
+   :report-to-file "target/junit.xml"})
 
--- src/jmxspy/core.clj
+++ src/jmxspy/core.clj
@@ -9,7 +9,8 @@
             [say-cheez.core :refer [capture-build-env-to]])
   (:gen-class))
 
-(capture-build-env-to BUILD)
+;(capture-build-env-to BUILD)
+(def BUILD {})
 
 (def ^:dynamic *ERRORLOG* "")
 
@@ -18,7 +19,14 @@
    (new java.text.SimpleDateFormat "yyyy-MM-dd HH:mm:ss")
    (java.util.Date.)))
 
+
+(defn timerFn []
+  (let [t0 (System/currentTimeMillis)]
+    (fn []
+      (- (System/currentTimeMillis) t0))))
+
 (defn logErr
+  "As we use STDOUT for JSON, we need to push notifications to a file."
   [& msg]
 
   (if (not (empty? *ERRORLOG*))
@@ -28,12 +36,14 @@
       (spit *ERRORLOG* fullMsg :append true))))
 
 (defn string->attr
-  [attribute]
+  "Transforms a string attribute that may include a
+   sub.-attribute into a vector of [:attr :subattr] "
+  [attributeName]
 
-  (let [[_ a sa] (re-matches #"(.+)\.(.+)" attribute)]
-    (if a ;; successful match
-      [(keyword a) (keyword sa)]
-      [(keyword attribute) nil])))
+  (let [[_ attr subattr] (re-matches #"(.+)\.(.+)" attributeName)]
+    (if attr ;; successful match
+      [(keyword attr) (keyword subattr)]
+      [(keyword attributeName) nil])))
 
 (defn read-bean
   "Examples:
@@ -41,16 +51,26 @@
   With sub-attribute
   (read-bean \"java.lang:type=Memory\" \"HeapMemoryUsage.max\")
 
-  " [bn attribute]
-  (let [[attr subattr] (string->attr attribute)]
+  " [bean attributeName]
+  (let [[attr subattr] (string->attr attributeName)]
     (cond
       (nil? subattr)
-      (jmx/read bn attr)
+      (jmx/read bean attr)
 
       :else
-      (get (jmx/read bn attr) subattr))))
+      (get (jmx/read bean attr) subattr))))
 
-(defn read-bean-attr [defaultMap {:keys [bean attribute as]}]
+(defn read-bean-attr
+  "Reads a bean by receiving our input structures:
+
+  - a defaults map
+  - the  bean itself
+
+  Returns a map of the bean values merged with the defaults.
+  If reading goes bad, the :value attribute is set to :ERR
+
+  "
+  [exraAttrs {:keys [bean attribute as]}]
   (let [v (try
             (read-bean bean attribute)
             (catch Exception e
@@ -58,12 +78,23 @@
                 (logErr "Bean: '" bean "'  Attr: '" attribute "' Error: " e)
                 :ERROR)))]
 
-    (into defaultMap {:metrics as :value v})))
+    (into exraAttrs {:metrics as :value v})))
 
-(defn mkStatus
+(s/fdef
+  read-bean-attr
+  :args (s/cat :defaults map?
+               :bean ::S/a-bean))
+
+
+(defn mk-jmx-error-status
+  "Builds the fake entry 'jmx_error_status' that ia set to 0
+  if everything okay and 1 if anything went south.
+
+  "
   [isOk? extraAttrs]
 
-  (into extraAttrs {:metrics "jmx_error_status"
+  (into extraAttrs
+        {:metrics "jmx_error_status"
                     :value (if isOk? 0 1)}))
 
 (defn beans-for
@@ -79,41 +110,58 @@
  :args (s/cat :cfg ::S/CFG
               :presets (s/coll-of keyword?)))
 
-(defn dump-server
-  [cfg {:keys [creds extra-attrs polling]}]
+(defn dump-all-beans
+  "Dumps all attributes for a server into a list.
+  It does not add the jmx-error-status but leaves :ERROR
+  entries in for further removal."
+
+  [cfg {:keys [extra-attrs polling]}]
   (let [allbeans (beans-for cfg polling)]
     (mapv (partial read-bean-attr extra-attrs) allbeans)))
 
 (defn dump-server-maybe-remote
+  "Dumps all benas on a server that is possibly remote.
+  After each server, we remove all :ERROR entries and add
+  a jmx_error_status entry.
+
+  If connection is impossible, we just log the event
+  and  add a jmx_error_status entry.
+
+  "
   [cfg {:keys [creds extra-attrs] :as all}]
 
   (try
     (let [beans (if (empty? creds)
-                  (dump-server cfg all)
+                  (dump-all-beans cfg all)
 
                   (jmx/with-connection creds
-                    (dump-server cfg all)))
+                                       (dump-all-beans cfg all)))
 
           have-errors? (some? (some #{:ERROR} (map :value beans)))
           error-free (filter #(not= :ERROR (:value %)) beans)]
 
-      (conj error-free (mkStatus (not have-errors?) extra-attrs)))
+      (conj error-free
+            (mk-jmx-error-status (not have-errors?) extra-attrs)))
 
     (catch Exception e
       (do
         (logErr "Connection issue " creds ":" e)
-        [(mkStatus false extra-attrs)]))))
+        [(mk-jmx-error-status false extra-attrs)]))))
 
 (defn dump-all-servers
   [{:keys [cfg errorlog]}]
 
   (binding [*ERRORLOG* errorlog]
     (let [_ (logErr "======= Starting ========")
+          timer (timerFn)
           servers (:servers cfg)
           data (reduce into []
                        (map (partial dump-server-maybe-remote cfg) servers))
           sorted-data (sort-by :metrics data)
-          _ (logErr "======= Ending ==========")]
+          _ (logErr "======= Ending: "
+                    " took: " (timer) " ms -"
+                    " # beans: " (count sorted-data)
+                    " ==========")]
 
       (println
        (json/generate-string sorted-data {:pretty true})))))
